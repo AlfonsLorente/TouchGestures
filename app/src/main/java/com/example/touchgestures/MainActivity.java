3105a6dec4dc412fcd0b6704d66136903edbd2bb
Alfons Lorente <alorente@cifpfbmoll.eu>
2021-10-14 12:33:47 +0200

    Cómo capturar eventos táctiles para objetos Activity o View

--- app/src/main/java/com/example/touchgestures/MainActivity.java
+++ app/src/main/java/com/example/touchgestures/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.touchgestures;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public boolean onTouchEvent(MotionEvent motionEvent){
+        int action = motionEvent.getActionMasked();
+        switch (action){
+            case(MotionEvent.ACTION_DOWN):
+                Toast.makeText(this, "Action was DOWN", Toast.LENGTH_SHORT).show();
+                return true;
+            case(MotionEvent.ACTION_MOVE):
+                Toast.makeText(this, "Action was MOVE", Toast.LENGTH_SHORT).show();
+                return true;
+            case(MotionEvent.ACTION_UP):
+                Toast.makeText(this, "Action was UP", Toast.LENGTH_SHORT).show();
+                return true;
+            case(MotionEvent.ACTION_CANCEL):
+                Toast.makeText(this, "Action was CANCEL", Toast.LENGTH_SHORT).show();
+                return true;
+            case(MotionEvent.ACTION_OUTSIDE):
+                Toast.makeText(this, "Movement occurred outside bounds " +
+                        "of current screen element", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onTouchEvent(motionEvent);
+
+        }
+    }
+
 }
